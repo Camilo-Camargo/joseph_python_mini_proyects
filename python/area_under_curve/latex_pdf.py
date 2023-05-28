250aeae0580c767e42741a0db8fa4f6f69126461
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2023-05-27 22:54:03 -0500

    create

---
+++ python/area_under_curve/latex_pdf.py
@@ -0,0 +1,48 @@
+import sympy as sp
+from pylatex import Document, Section, Subsection, Math, Command
+
+def main(var, fun, sup, inf):
+    # Variable de integración
+    variable = sp.Symbol(var)
+    # Definición de la función
+    funcion = sp.sympify(fun)
+    # Tipo de función
+    funcion_tipo = input('Es una función trigonométrica?: ')
+    if funcion_tipo == 'si':
+        # Intervalo de integración
+        a = inf*sp.pi  # Límite inferior del intervalo
+        b = sup*sp.pi # Límite superior del intervalo
+    else:
+        a = inf
+        b = sup
+    # Cálculo del área utilizando la función de SymPy
+    area = sp.integrate(funcion, (variable, a, b))
+
+    # Crear el documento LaTeX
+    doc = Document()
+
+    # Agregar sección
+    with doc.create(Section('Resultado')):
+        # Agregar expresión LaTeX renderizada
+        doc.append('Expresión:')
+        doc.append(Math(data=[sp.latex(funcion)]))
+        doc.append('Resultado:')
+        doc.append(Math(data=[sp.latex(area)]))
+
+    # Guardar el PDF
+    doc.generate_pdf('resultado', clean_tex=False)
+
+    # Mostrar el área calculada
+    print(f'El área bajo la curva es: {area}')
+
+    # Mostrar el nombre del archivo PDF
+    print('PDF guardado como "resultado.pdf"')
+
+if __name__ == "__main__":
+    sp.init_printing()
+    var = input('Digite la variable: ')
+    fun = input('Digite la función: ')
+    inf = int(input('Digite el valor del límite inferior: '))
+    sup = int(input('Digite el valor del límite superior: '))
+    main(var, fun, sup, inf)
+
